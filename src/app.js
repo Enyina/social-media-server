c148616c1800ab3a42460ab05698e5ee81882d61
Enyina Matthew <enyina3848@gmail.com>
2023-04-03 12:22:47 +0100

    add cors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const AppError = require("./utils/appError");
 const errorHandler = require("./middleware/errorHandler");
 const v1Router = require("./router");
 const { stream } = require("./config/winston");
+const cors = require(cors);
 
 const cookieParser = require("cookie-parser");
 
@@ -16,6 +17,7 @@ app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 //middleware
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
