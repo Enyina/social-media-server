6017aa71273c2309fd513d6d20f3eddd023107ee
Enyina Matthew <enyina3848@gmail.com>
2023-04-03 12:21:20 +0100

    add cors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -88,21 +88,20 @@ const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, req, res);
-  } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
-    error.message = err.message;
+  // if (process.env.NODE_ENV === "development") {
+  //   sendErrorDev(err, req, res);
+  // } else if (process.env.NODE_ENV === "production") {
+  let error = { ...err };
+  error.message = err.message;
 
-    if (error.name === "CastError") error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === "ValidationError")
-      error = handleValidationErrorDB(error);
-    if (error.name === "JsonWebTokenError") error = handleJWTError();
-    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+  if (error.name === "CastError") error = handleCastErrorDB(error);
+  if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+  if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+  if (error.name === "JsonWebTokenError") error = handleJWTError();
+  if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
-    sendErrorProd(error, req, res);
-  }
+  sendErrorProd(error, req, res);
+  // }
 };
 
 module.exports = errorHandler;
